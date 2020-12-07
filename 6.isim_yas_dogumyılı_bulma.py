7c3eae71cda90dc45ec1c4af88081c927407d1ac
yalcinbrvn <yalcinbrvn@gmail.com>
2020-12-08 01:33:50 +0300

    isim yaş ve doğum yılı bulma eklendi.

---
+++ 6.isim_yas_dogumyılı_bulma.py
@@ -0,0 +1,4 @@
+isim=str(input("isminizi giriniz: "))
+yas=int(input("Yaşınızı giriniz: "))
+dogumyılı=int(input("Doğum yılınızı giriniz: "))
+print("İsminiz:",isim,"Yaşınız:",yas,"Doğum yılınız:",dogumyılı)
