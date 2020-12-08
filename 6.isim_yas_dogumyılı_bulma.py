bef6c34afeaa9f9f967bb050ebb26a74ea06c2ed
yalcinbrvn <yalcinbrvn@gmail.com>
2020-12-08 12:58:31 +0300

    verilen doğum yılından şuan ki yaşı hesaplayan program.

--- 6.isim_yas_dogumyılı_bulma.py
+++
@@ -1,4 +0,0 @@
-isim=str(input("isminizi giriniz: "))
-yas=int(input("Yaşınızı giriniz: "))
-dogumyılı=int(input("Doğum yılınızı giriniz: "))
-print("İsminiz:",isim,"Yaşınız:",yas,"Doğum yılınız:",dogumyılı)
