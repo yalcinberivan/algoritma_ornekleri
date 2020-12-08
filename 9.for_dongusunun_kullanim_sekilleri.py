c75b63841d0d01a456ff3c11124959bea29aafc7
yalcinbrvn <yalcinbrvn@gmail.com>
2020-12-08 17:47:40 +0300

    for döngüsünün kullanım şekilleri eklendi.

---
+++ 9.for_dongusunun_kullanim_sekilleri.py
@@ -0,0 +1,13 @@
+for i in range(10):
+    print("%d\t"%i,end="")
+print()
+for i in range(1,10):
+    print("%d\t"%i,end="")
+print()
+for i in range(1,10,2):
+    print("%d\t"%i,end="")
+print()
+for i in range(10,0,-2):
+    print("%d\t"%i,end="")
+    
+    
