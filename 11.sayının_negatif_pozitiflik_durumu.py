a7fa763201acbb958b93ec14df11fe5960713a98
yalcinbrvn <yalcinbrvn@gmail.com>
2020-12-08 18:10:28 +0300

    girilen sayının işaret durumu eklendi.

---
+++ 11.sayının_negatif_pozitiflik_durumu.py
@@ -0,0 +1,7 @@
+sayi=float(input("Bir sayı giriniz: "))
+if sayi > 0:
+    print("Girilen sayı pozitiftir.")
+elif sayi < 0:
+    print("Girilen sayı negatiftir.")
+else:
+    print("Sayı ne negatif ne de pozitiftir.")
