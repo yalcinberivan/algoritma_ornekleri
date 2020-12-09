2bfc3d982a588f1492798e8741a280f0ec7bb048
yalcinbrvn <yalcinbrvn@gmail.com>
2020-12-09 19:24:31 +0300

    dörtgenin alanı eklendi.

--- 17.dortgenin_alanı.py
+++ 17.dortgenin_alanı.py
@@ -2,6 +2,6 @@ import math
 e=float(input("Birinci köşegeni giriniz: "))
 f=float(input("İkinci köşegeni giriniz: "))
 aci=int(input("Aradaki açıyı giriniz: "))
-pi=3.14
-alan=e*f*math.sin(aci*pi/180)/2
+math.pi
+alan=e*f*math.sin(aci*math.pi/180)/2
 print("Dörtgenin alanı: {:.2f}\n ".format(alan))
