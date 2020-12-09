cf57674cdd0050177cc4f820ad1f818cd213cb71
yalcinbrvn <yalcinbrvn@gmail.com>
2020-12-10 02:06:04 +0300

    transfarmatörün akım gücü hesaplaması eklendi.

--- 20-30 arası örnekler/28.transformator-akim-guc-hesaplama.py
+++ 20-30 arası örnekler/28.transformator-akim-guc-hesaplama.py
@@ -1,1 +1,13 @@
-##kod yazılacak
+vp=int(input("Tranfarmatörün primer gerilimi giriniz: "))
+Ip=int(input("Tranfarmatörün akım gerilimi giriniz: "))
+np=int(input("Tranfarmatörün sarım sayısı gerilimini giriniz: "))
+ns=int(input("Tranfarmatörün seconder gerilimi giriniz: "))
+
+vs=vp*ns/np
+Is=Ip*np/ns
+ps=vs*Is
+
+if vs>vp :
+    print("Yükseltici")
+else:
+    print("Düşürücü")
