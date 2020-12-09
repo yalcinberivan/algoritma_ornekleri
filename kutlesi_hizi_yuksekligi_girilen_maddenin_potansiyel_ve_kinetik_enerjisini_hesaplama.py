2d4cadbd974d98fb6ff37c80d014a40c855c3389
yalcinbrvn <yalcinbrvn@gmail.com>
2020-12-09 19:42:49 +0300

    maddenin kinetik ve potansiyel enerjisini hesaplama eklendi.

---
+++ kutlesi_hizi_yuksekligi_girilen_maddenin_potansiyel_ve_kinetik_enerjisini_hesaplama.py
@@ -0,0 +1,12 @@
+m=int(input("Maddenin kütlesini giriniz (m) : "))
+v=int(input("Maddenin hızını giriniz (v) :"))
+h=int(input("Yüksekliği giriniz (h) :"))
+
+ep=m*9.8*h
+ek=(m*v**2)/2
+
+print("potansiyel enerji: ",ep)
+print("Kinetik enerji: ",ek)
+
+
+
