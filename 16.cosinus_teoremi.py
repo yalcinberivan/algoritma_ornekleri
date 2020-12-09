28db8b19ce913fce29388e209971201d2484d6c6
yalcinbrvn <yalcinbrvn@gmail.com>
2020-12-09 19:20:26 +0300

    cosinüs teoremi girildi.

--- 16.cosinus_teoremi.py
+++ 16.cosinus_teoremi.py
@@ -2,6 +2,6 @@ import math
 a=float(input("Birinci kenarı giriniz: "))
 b=float(input("İkinci kenarı giriniz: "))
 aci=int(input("Aradaki açıyıy giriniz: "))
-pi=3.14
-c=a**2+b**2-2*a*b*math.cos(aci*pi/180)**1/2
+math.pi
+c=a**2+b**2-2*a*b*math.cos(aci*math.pi/180)**1/2
 print("Sonuç: ",c)
