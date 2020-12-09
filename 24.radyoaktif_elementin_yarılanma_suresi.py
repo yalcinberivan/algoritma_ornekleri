2dc9a82f728b01db4c20f6ca2ba752e1251492a9
yalcinbrvn <yalcinbrvn@gmail.com>
2020-12-09 21:52:28 +0300

    ohm kanunu eklendi.

---
+++ 24.radyoaktif_elementin_yarılanma_suresi.py
@@ -0,0 +1,7 @@
+mo=int(input("Başlangıç kütlesini giriniz: "))
+i=int(input("Yarılanma süresini giriniz: "))
+t=int(input("Süreyi giriniz: "))
+n=t/dt
+m=mo/2**n
+print("Yarılama sayısı:" ,n)
+print("Kalan kütle: ",m)
---
+++ 25.ohm_kanunu.py
@@ -0,0 +1,4 @@
+r=int(input("İletkenin direncini giriniz: "))
+i=int(input("İletkenin içinden akan akımı giriniz: "))
+v=r*i
+print("Akımın uçlarındaki gerilim: ",v)
