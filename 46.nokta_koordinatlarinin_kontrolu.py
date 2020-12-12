aeae3841be8679c395cbd34237957abdf0be1334
yalcinbrvn <yalcinbrvn@gmail.com>
2020-12-12 14:19:17 +0300

    nokta koordinatlarının kontrolü eklendi.

---
+++ 46.nokta_koordinatlarinin_kontrolu.py
@@ -0,0 +1,11 @@
+x1=int(input("X1 noktasını giriniz: "))
+y1=int(input("Y1 noktasını giriniz: "))
+x2=int(input("X2 noktasını giriniz: "))
+y2=int(input("Y2 noktasını giriniz: "))
+x3=int(input("X3 noktasını giriniz: "))
+y3=int(input("Y3 noktasını giriniz: "))
+
+if (x3>x1) and (y3>y1) and (x3<x2) and (y3<y2):
+    print("İçinde")
+else:
+    print("İçinde değil")
