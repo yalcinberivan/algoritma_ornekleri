29e3e7955b7e35a47da81a74247d98251ba3347f
yalcinbrvn <yalcinbrvn@gmail.com>
2020-12-13 03:13:35 +0300

    linting

--- c/1-10 arası örnekler/2_iki_sayinin_carpimi.c
+++ c/1-10 arası örnekler/2_iki_sayinin_carpimi.c
@@ -3,7 +3,8 @@
 int sayi1,sayi2,carpim;
 main()
 {
-	printf("Birinci sayiyi giriniz: ");scanf("%d",&sayi1);
+
+    printf("Birinci sayiyi giriniz: ");scanf("%d",&sayi1);
     printf("Ikinci sayiyi giriniz: ");scanf("%d",&sayi2);
 
     carpim = sayi1 * sayi2;
