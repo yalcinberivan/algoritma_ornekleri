665c703541d2e29b44b96a7b650605c6c7e5cc55
yalcinbrvn <yalcinbrvn@gmail.com>
2020-12-13 14:49:14 +0300

    mutlak değer eklendi.

---
+++ c/1-10 arası örnekler/3_mutlak_deger.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int sayi;
+
+int main(){
+
+    printf("Lutfen bir sayi giriniz: ");scanf("%d",&sayi);
+
+    printf("%d", abs(sayi));
+
+    return 0;
+}
