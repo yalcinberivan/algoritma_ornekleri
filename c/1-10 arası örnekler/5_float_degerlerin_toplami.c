db181ccc277d92d6d90774ee0ea89b5ee97e4a4e
yalcinbrvn <yalcinbrvn@gmail.com>
2020-12-13 16:06:46 +0300

    float değerleirn toplami eklendi.

---
+++ c/1-10 arası örnekler/5_float_degerlerin_toplami.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+int sayi1,sayi2,toplam;
+
+printf("Birinci sayiyi giriniz: ");scanf("%d",&sayi1);
+
+printf("Ikinci sayiyi giriniz: ");scanf("%d",&sayi2);
+
+toplam=sayi1+sayi2;
+
+printf("Sayilarin toplami=%d\n",toplam);
+
+return 0;
+}
