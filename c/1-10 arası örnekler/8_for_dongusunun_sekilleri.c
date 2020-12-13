b40f009b5aeba77ba4f6f6bc91d98cee66206300
yalcinbrvn <yalcinbrvn@gmail.com>
2020-12-13 22:02:42 +0300

    for döngüsünün şekilleri eklendi.

---
+++ c/1-10 arası örnekler/8_for_dongusunun_sekilleri.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+
+int i = 0;
+for (i=0;i<=10;i++){
+printf("%d--",i);
+}
+
+printf("\n");
+
+int j = 0;
+for (j=1;j<10;j++){
+printf("%d--",j);
+}
+
+
+printf("\n");
+
+int k = 0;
+for (k=1;k<=10;k+=2){
+printf("%d--",k);
+}
+
+printf("\n");
+
+int l = 0;
+for (l=0;l<=10;l+=2){
+printf("%d--",l);
+}
+
+
+return 0;
+    }
