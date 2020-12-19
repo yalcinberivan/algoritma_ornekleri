f4d98b329ebe77b4870300df39a85ea5289403c0
yalcinbrvn <yalcinbrvn@gmail.com>
2020-12-19 19:41:58 +0300

    transformatörün akım ve güç hesaplanması eklendi.

---
+++ c/20-30 arası örnekler/28_transformator_akim_guc_hesaplanmasi.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int vp, ip, np, ns, vs, is, ps;
+    printf("Transfarmatorun primer gerilimini giriniz: ");
+    scanf("%d", &vp);
+    printf("Transfarmatorun akim gerilimini giriniz: ");
+    scanf("%d", &ip);
+    printf("Tranformatorun sarim sayisi gerilimini giriniz: ");
+    scanf("%d", &np);
+    printf("Tranformatorun seconder gerilimi giriniz: ");
+    scanf("%d", &ns);
+    vs = vp * ns / np;
+    is = ip * np / ns;
+    ps = vs * is;
+    if (vs > vp)
+        printf("Yukseltici tranfarmator");
+    else
+        printf("Dusurucu transfarmator");
+
+    return 0;
+}
