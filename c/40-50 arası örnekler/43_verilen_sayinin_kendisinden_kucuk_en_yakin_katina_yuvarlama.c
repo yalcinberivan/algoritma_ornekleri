40de9dfec73ee918d4e00eaa6bc525eba4f44a8f
yalcinbrvn <yalcinbrvn@gmail.com>
2020-12-21 22:09:49 +0300

    verilen sayıyı en yakın sayıya yuvarlama

---
+++ c/40-50 arası örnekler/43_verilen_sayinin_kendisinden_kucuk_en_yakin_katina_yuvarlama.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int s;
+    printf("Tamsayi:");
+    scanf("%d", &s);
+    printf("\n%d", (((int)((s * 2) / 10)) * 5));
+
+    return 0;
+}
