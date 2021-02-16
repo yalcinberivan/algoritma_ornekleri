873b09220037b48a8c60779230150330ed60fa6c
yalcinbrvn <yalcinbrvn@gmail.com>
2021-02-16 14:28:26 +0300

    kod düzenlendi gereksiz boşluklar silindi.

--- c/10-20 arası örnekler/19_daire_diliminn_alani.c
+++ c/10-20 arası örnekler/19_daire_diliminn_alani.c
@@ -3,14 +3,16 @@
 
 int main()
 {
+    
 #define PI 3.14
-    int r, aci, alan;
-    printf("Dairenin yaricapini giriniz: ");
-    scanf("%d", &r);
-    printf("Aradaki aciyi giriniz: ");
-    scanf("%d", &aci);
-    alan = (aci * PI * pow(r, 2)) / 360;
-    printf("Daire diliminin alani: %d\n", alan);
+int r, aci, alan;
+
+printf("Dairenin yaricapini giriniz: ");
+scanf("%d", &r);
+printf("Aradaki aciyi giriniz: ");
+scanf("%d", &aci);
+alan = (aci * PI * pow(r, 2)) / 360;
+printf("Daire diliminin alani: %d\n", alan);
 
     return 0;
-}
+    }
