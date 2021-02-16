75cecba5d9aea19c2d8d460e946bdeb7368cc879
yalcinbrvn <yalcinbrvn@gmail.com>
2021-02-16 14:47:38 +0300

    sayıyı kendisinden küçük en yakın 5'in kat. yuvar.

--- c/40-50 arası örnekler/43_verilen_sayinin_kendisinden_kucuk_en_yakin_katina_yuvarlama.c
+++ c/40-50 arası örnekler/43_verilen_sayinin_kendisinden_kucuk_en_yakin_katina_yuvarlama.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <conio.h>
+
 int main()
 {
-    int s;
-    printf("Tamsayi:");
-    scanf("%d", &s);
-    printf("\n%d", (((int)((s * 2) / 10)) * 5));
+
+int s;
+
+printf("Tamsayi:");
+scanf("%d", &s);
+printf("\n%d", (((int)((s * 2) / 10)) * 5));
 
     return 0;
-}
+    }
