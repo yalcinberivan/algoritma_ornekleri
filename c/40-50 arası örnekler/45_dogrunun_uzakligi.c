46126e0172d559a3483749449a22b61edb488f93
yalcinbrvn <yalcinbrvn@gmail.com>
2021-02-16 15:36:14 +0300

    gerekli düzenlemeler yapıldı.

--- c/40-50 arası örnekler/45_dogrunun_uzakligi.c
+++ c/40-50 arası örnekler/45_dogrunun_uzakligi.c
@@ -3,22 +3,24 @@
 
 int main()
 {
-    int a, b, c, x1, y1, uz, uz1;
-    printf("X'in katsayisini giriniz: ");
-    scanf("%d", &a);
-    printf("Y'nin katsayisini giriniz: ");
-    scanf("%d", &b);
-    printf("Sabiti giriniz: ");
-    scanf("%d", &c);
-    printf("X1 noktasini giriniz: ");
-    scanf("%d", &x1);
-    printf("Y1 noktasini giriniz: ");
-    scanf("%d", &y1);
 
-    if (a * x1 + b * y1 + c == 0)
+int a, b, c, x1, y1, uz, uz1;
 
-        printf("Girilen nokta dogrunun uzerindedir.");
+printf("X'in katsayisini giriniz: ");
+scanf("%d", &a);
+printf("Y'nin katsayisini giriniz: ");
+scanf("%d", &b);
+printf("Sabiti giriniz: ");
+scanf("%d", &c);
+printf("X1 noktasini giriniz: ");
+scanf("%d", &x1);
+printf("Y1 noktasini giriniz: ");
+scanf("%d", &y1);
+
+if (a * x1 + b * y1 + c == 0)
+    printf("Girilen nokta dogrunun uzerindedir.");
+else
     uz = abs(a * x1 + b * y1 + c) / pow((a * a + b * b), 0.5);
     printf("Girilen nokta,dogru uzerinde degildir.Noktanin dogruya uzakligi %d birimdir.", uz);
     return 0;
-}
+    }
