64999915da326da99b40f15f1f8b10db8ea12833
yalcinbrvn <yalcinbrvn@gmail.com>
2021-02-16 14:57:29 +0300

    gerekli düzenlemeler yapıldı.

--- c/10-20 arası örnekler/18_kurenin_alani_hacmi.c
+++ c/10-20 arası örnekler/18_kurenin_alani_hacmi.c
@@ -5,15 +5,17 @@ int main()
 {
 
 #define PI 3.14
-    int r, alan, hacim;
-    printf("Yaricapi giriniz: ");
-    scanf("%d",&r);
-    alan = 4 * PI * pow(r, 2);
-    hacim=(4*PI*pow(r,3))/3;
+int r, alan, hacim;
+
+printf("Yaricapi giriniz: ");
+scanf("%d",&r);
+
+alan = 4 * PI * pow(r, 2);
+hacim=(4*PI*pow(r,3))/3;
 
 printf("Kurenin hacmi: %d\n",hacim);
 printf("Kurenin alani: %d\n",alan);
 
 
     return 0;
-}
+    }
