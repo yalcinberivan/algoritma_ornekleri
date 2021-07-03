d234a425efbb8b655a67d5426aa46f69b9f40136
yalcinberivan <73023157+yalcinberivan@users.noreply.github.com>
2021-07-03 21:54:21 +0300

    Delete 95_heterometrik_sayilar.py

--- python/90-100 arası örnekler/95_heterometrik_sayilar.py
+++
@@ -1,8 +0,0 @@
-"""Heterometrik sayı:a pozitif bir tamsayı olmak üzere a(a+1) ile elde edilebilen sayılara "heterometrik sayı" denir
-ör:2=1.2,6=2.3,12=3.4 bu sayılardan bazılarıdır"""
-#klavyeden girilen adet sayısınca "heterometrik sayıları oluşturan ve bunları açık yazarak alt alta listeleyen program. 
-
-sayi=int(input("Tamsayı giriniz: "))
-for a in range (1,sayi,1):
-    print(a,"x",a+1,"=",a*(a+1))
-    
