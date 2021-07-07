a271f69d53abff283d0703b491c32af2eeb0cdcf
yalcinbrvn <yalcinbrvn@gmail.com>
2021-07-07 20:57:38 +0300

    n basamak sayısına göre palindrome sayılarının toplamının bulunması.

---
+++ python/90-100 arası örnekler/100_n_basamak_sayisina_gore_palindrome_tamsayilarin_toplam_sayisinin_bulunmasi.py
@@ -0,0 +1,3 @@
+"""klavyeden girilen n basamak sayısına göre palindrome tamsayıların toplam sayısını bulup yazdıran program."""
+basamakSayisi=int(input("Basamak sayısını giriniz: "))
+print(9*10**((basamakSayisi-1)/2))
